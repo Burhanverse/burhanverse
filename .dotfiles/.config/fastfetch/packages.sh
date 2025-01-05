30568d4f9810f1253e14c1bdcf7d9cb0d0ef4a7e
Burhanverse <burhanverse@gmail.com>
2025-01-05 15:06:21 +0530

    update

--- .dotfiles/.config/fastfetch/packages.sh
+++ .dotfiles/.config/fastfetch/packages.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-# Get RPM package count
-rpm_count=$(rpm -qa | wc -l)
-
-# Get Flatpak package count
-flatpak_count=$(flatpak list 2>/dev/null | wc -l)
-
-echo "$rpm_count (RPM), $flatpak_count (Flatpak)"
+cat ~/.config/fastfetch/packages_count.txt
